0b78d782a43f224230a2fb3ca1300fbd811e90d5
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-17 01:32:31 +0300

    Fibonacci generator and some tests created

---
+++ Task1.LogicFibonacci/FibonacciGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.LogicFibonacci
+{
+    public static class FibonacciGenerator
+    {
+        /// <summary>
+        /// Creates sequence of Fibonacci nubmers
+        /// </summary>
+        public static IEnumerable<long> GetFibonacciSequence()
+        {
+            yield return 1;
+            yield return 1;
+            long pred = 1;
+            long cur = 1;
+            while (true)
+            {
+                long temp;
+                try
+                {
+                    temp = checked(pred + cur);
+                }
+                catch (OverflowException)
+                {
+                    yield break;
+                }
+                yield return temp;
+                pred = cur;
+                cur = temp;
+            }
+        }
+    }
+}
---
+++ Task1.Tests/FibonacciGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1.LogicFibonacci;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class FibonacciGeneratorTests
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [Test]
+        public void GetFibonacciSequence_Pos_NumberExpected(int pos, long expectedFibonacciNumber)
+        {
+            IEnumerator<long> fibSeq = FibonacciGenerator.GetFibonacciSequence().GetEnumerator();
+            bool b = false;
+            for (int i = 0; i < pos; i++)
+                b = fibSeq.MoveNext();
+            if (!b)
+                Assert.Fail($"{pos} fibonacci number cannot be method");
+            Assert.AreEqual(expectedFibonacciNumber, fibSeq.Current, $"{nameof(pos)} = {pos}");
+        }
+    }
+}
