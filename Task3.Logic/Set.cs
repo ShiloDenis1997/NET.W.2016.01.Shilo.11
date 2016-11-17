476e5e308587bc087911d9471b2b6c0505242051
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-17 13:44:43 +0300

    Task3.Logic project in progress

---
+++ Task3.Logic/Set.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Logic
+{
+    public class Set<T> : ISet<T> where T : class, IEquatable<T>
+    {
+        private T[] array;
+        private IEqualityComparer<T> equalityComparer;
+
+        public Set(int capacity = 50, IEqualityComparer<T> equalityComparer = null)
+        {
+            if (capacity <= 0)
+                throw new ArgumentException(
+                    $"{nameof(capacity)} is less or equal to zero");
+            array = new T[capacity];
+            this.equalityComparer = equalityComparer;
+        }
+
+        public Set(IEnumerable<T> elements, IEqualityComparer<T> equalityComparer,
+            int capacity = 50)
+        {
+            if (elements == null)
+                throw new ArgumentNullException
+                    ($"{nameof(elements)} parameter is null");
+            if (capacity <= 0)
+                throw new ArgumentException(
+                    $"{nameof(capacity)} is less or equal to zero");
+            UnionWith(elements);
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
+
+        public bool Add(T item)
+        {
+            if (Contains(item))
+                return false;
+            if (Count == array.Length)
+                ResizeArray(array.Length*2);
+            array[Count++] = item;
+            return true;
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            if (ReferenceEquals(other, this))
+                return;
+
+            foreach (T el in other)
+            {
+                Add(el);
+            }
+        }
+
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            if (ReferenceEquals(other, this)) 
+                return;
+            if (Count == 0)
+                return;
+            int count = 0;
+            T[] temp = new T[array.Length];
+            foreach (T el in other)
+            {
+                if (Remove(el))
+                {
+                    temp[count++] = el;
+                }
+            }
+            array = temp;
+            Count = count;
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            if (Count == 0)
+                return;
+            foreach (T el in other)
+            {
+                Remove(el);
+            }
+        }
+
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            if (Count == 0)
+                UnionWith(other);
+            if (ReferenceEquals(other, this))
+            {
+                Clear();
+            }
+            Set<T> set = new Set<T>(other, equalityComparer);
+            SymmetricExceptWithSet(set);
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            int count = 0;
+            Set<T> set = new Set<T>(other, equalityComparer);
+            foreach(T el in set)
+                if (Contains(el))
+                    count++;
+            if (count == Count)
+                return true;
+            return false;
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            foreach (T el in other)
+                if (!Contains(el))
+                    return false;
+            return true;
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            int count = 0;
+            Set<T> set = new Set<T>(other, equalityComparer);
+            foreach (T el in set)
+                if (!Contains(el))
+                    return false;
+                else
+                {
+                    count++;
+                }
+            if (count == Count)
+                return false;
+            return true;
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            int count = 0;
+            bool canBeSubset = false;
+            Set<T> set = new Set<T>(other, equalityComparer);
+            foreach (T el in set)
+                if (Contains(el))
+                    count++;
+                else
+                {
+                    canBeSubset = true;
+                }
+            if (count == Count && canBeSubset)
+                return true;
+            return false;
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            foreach (T el in other)
+                if (Contains(el))
+                    return true;
+            return false;
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException($"{nameof(other)} is null");
+            int count = 0;
+            Set<T> set = new Set<T>(other, equalityComparer);
+            foreach (T el in set)
+                if (Contains(el))
+                    count++;
+                else
+                    return false;
+            if (count == Count)
+                return true;
+            return false;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            array = new T[array.Length];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                bool compareResult;
+                if (equalityComparer != null)
+                    compareResult = equalityComparer.Equals(item, array[i]);
+                else
+                    compareResult = array[i].Equals(item);
+                if (compareResult)
+                    return false;
+            }
+            return true;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void ResizeArray(int capacity)
+        {
+            T[] temp = new T[capacity];
+            Array.Copy(array, temp, Count);
+            array = temp;
+        }
+
+        private void SymmetricExceptWithSet(Set<T> other)
+        {
+            foreach (T el in other)
+            {
+                if (!Remove(el))
+                    Add(el);
+            }
+        }
+    }
+}
+
